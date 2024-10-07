dd8734ea455e623dbe4aac101fefd5b601f80919
Tetiana <tatiana.makarenko94@gmail.com>
2024-10-07 15:52:31 +0300

    Update js file

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,18 @@
-function getElementWidth(content, padding, border) {
-  let contentWidth = parseFloat(content);
-  let paddingWidth = parseFloat(padding) * 2;
-  let borderWidth = parseFloat(border) * 2;
-  let totalWidth = contentWidth + paddingWidth + borderWidth;
-  return totalWidth;
+function filterArray(numbers, value) {
+  let numbersArr = [];
+  let index = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      numbersArr[index] = numbers[i];
+      index++;
+    }
+  }
+  return numbersArr;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
